24fdb103bbd8def7e6a3fe640b70aadf84b2731f
Adib-96 <adibbh1996@gmail.com>
2023-09-28 16:39:08 -0700

    brain fuck

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
 for i in range(10):
     for j in range(10):
         if j < i or j == i:
             continue
         else:
             if i == 8:
-                print("{}{}".format(i, j), end=" ")
+                print("{}{}".format(i, j),end=" ")
             else:
                 print("{}{}".format(i, j), end=", ")
 print()
