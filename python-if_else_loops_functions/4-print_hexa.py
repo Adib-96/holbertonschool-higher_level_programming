5caba7b6112be88f5dcdf586994e68553f843929
Adib-96 <adibbh1996@gmail.com>
2023-09-28 11:07:04 -0700

    Hexadecimal printing

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for character in range(0, 99):
-    print(" {} = 0x{:x}".format(character, character))
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
