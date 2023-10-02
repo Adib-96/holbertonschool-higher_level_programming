5c8d4e07ae74f29a88e545d9ed44d5b4f703179b
Adib-96 <adibbh1996@gmail.com>
2023-10-02 16:03:38 -0700

    My firsttoolboxpycodestyle calculator_1.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,6 +3,6 @@ import calculator_1 as c
 a = 10
 b = 5
 arr = [c.add, c.sub, c.mul, c.div]
-sym = ['+','-','*','/']
+sym = ['+', '-', '*', '/']
 for i in range(4):
     print(f'{a} {sym[i]} {b} = {arr[i](a, b)}')
