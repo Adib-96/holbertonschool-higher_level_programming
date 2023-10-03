3f8ca0359c88dd892fbc53c9437e11b5567c6e12
Adib-96 <adibbh1996@gmail.com>
2023-10-03 05:18:31 -0700

    My first toolbox!

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import calculator_1 as c
-a = 10
-b = 5
-arr = [c.add, c.sub, c.mul, c.div]
-sym = ['+', '-', '*', '/']
-for i in range(4):
-    print(f'{a} {sym[i]} {b} = {arr[i](a, b)}')
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
