114e891f605caec73e807113841d755d7e1ff62a
Adib-96 <adibbh1996@gmail.com>
2023-10-06 08:03:56 -0700

    Find the max

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = my_list[0]
+    if len(my_list) <= 0:
+        return None
+    for num in range(1, len(my_list)):
+        if max < my_list[num]:
+            max = my_list[num]
+    return max
