21c26a61858efb16bb3e0c092180aa7ef47fa684
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-07 09:38:08 +0100

    delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,4 @@
-def delete_at(my_list=[], idx=0):
+def delete_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
-        return my_list  
-    new_list = my_list[:idx] + my_list[idx+1:]
-    
-    return new_list
+        return
+    del my_list[idx]
