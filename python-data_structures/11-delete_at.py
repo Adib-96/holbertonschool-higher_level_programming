9fc34a8bcb35b2a7ff63c159e12559bf239cad73
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-07 09:35:01 +0100

    delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,9 @@
 def delete_at(my_list=[], idx=0):
-    if idx <0 or idx == len(my_list):
-        return  my_list
-    else:
-        del my_list[idx]
-        return my_list
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return my_list  # Return the same list without changes
+    
+    # Create a new list with the items before and after the specified index
+    new_list = my_list[:idx] + my_list[idx+1:]
+    
+    return new_list
