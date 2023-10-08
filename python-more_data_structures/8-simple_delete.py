3c0bb96d3352c6a2127498cd9422eb3df25c2655
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-08 02:26:18 +0100

    delete by key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,10 @@
 #!usr/bin/python3
 def simple_delete(a_dictionary, key=""):
+    # Check if the key exists in the dictionary
     if key in a_dictionary:
+        # Delete the key-value pair from the dictionary
         del a_dictionary[key]
-    else:
-        return a_dictionary
+    # Return the updated dictionary
+    return a_dictionary
+
     
