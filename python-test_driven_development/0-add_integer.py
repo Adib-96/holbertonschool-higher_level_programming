00423eabfe305b1e827d5a9043843ae2b410c76c
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-15 23:44:46 +0100

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,15 +2,15 @@
 
 def add_integer(a, b=98):
     """"Add integer a to b in normal case.
-    
+
     ----float args are typecasted to integer
-    
+
     Raises:
         - TypeError: if either arguments is non-integer and non-float
 
     """
-    if ((not isinstance(a,int) and not isinstance(a,float))):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b,int) and not isinstance(b,float))):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
