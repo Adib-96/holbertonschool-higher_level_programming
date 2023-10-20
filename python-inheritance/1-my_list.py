d82cf98ab98bb1151c036269504cf67341a47289
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-20 23:36:02 +0100

    MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-"""Define a class(MyList) that inherits from list classe"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
     """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
-        """Sort a list and print it"""
-        print(self.sort())
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
