bade4f33388e4f913c268ebc1ea6a3da8131e82a
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-25 23:59:24 +0100

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that create a function that read file and return."""
+
 def read_file(filename=""):
     """Read file from specified directory"""
     with open(filename, 'r', encoding="UTF8") as f:
