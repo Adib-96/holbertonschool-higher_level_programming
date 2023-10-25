5aa562b059ee16113a1bb387f12103a213ab604b
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-25 18:18:59 +0100

    Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -60,9 +60,9 @@ class Square:
         if self.__size == 0:
             print()
             return
-        else:
-            [print("") for i in range(0, self.__position[1])]
-            for i in range(0, self.__size):
-                [print(" ", end="") for j in range(0, self.__position[0])]
-                [print("#", end="") for k in range(0, self.__size)]
-                print("")
+        
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
