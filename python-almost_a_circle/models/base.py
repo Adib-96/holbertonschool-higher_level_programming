7505da3af23e097b8b811a16c5facc83146983d7
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-25 23:06:43 +0100

    Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module with a Base class 
+"""
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new instance of the Base class"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
