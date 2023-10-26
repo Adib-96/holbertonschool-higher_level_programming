b058521d71deb1c434de2ee5fc11909703a0ed77
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-26 13:58:30 +0100

    To JSON String

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+
+"""
+Function that handles JSON representation
+"""
+
+
+def to_json_string(my_obj):
+    """function that convert Python to JSON :)"""
+    return json.dumps(my_obj)
