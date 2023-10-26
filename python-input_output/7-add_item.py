ec5bd5c606e63331ac74cdb16af38364442ce3a8
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-26 15:06:37 +0100

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,6 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 open("add_item.json", "a")
-
+my_list =[]
 my_list = load_from_json_file("add_item.json")
 save_to_json_file(my_list + sys.argv[1:], "add_item.json")
