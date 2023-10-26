ac3709f0ac2b064947d565ed98d9b0011f75df10
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-26 13:40:24 +0100

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -10,6 +10,4 @@ def write_file(filename="", text=""):
     """
     with open(filename, 'w', encoding="utf-8") as f:
         char_count = f.write(text)
-        print(f.write(text), end='')
-    
     return char_count
