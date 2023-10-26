2f825ed7dafd04038370e9b9093919f8e9a21f83
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-26 13:37:19 +0100

    write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that write a string to a text file
+"""
+
+
+def read_file(filename="", text=""):
+    """
+    write string to a text file and return the number of charcter written
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        char_count = f.write(text)
+        print(f.write(text), end='')
+    
+    return char_count
