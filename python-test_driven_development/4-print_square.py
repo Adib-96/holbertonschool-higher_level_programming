c75ddb4b18550a3d7c42453c8b178b128a934190
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-28 20:28:10 +0100

    Print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Module that define a function to print a square matrix."""
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """Print a square matrix.
+    """Print a square matrix with the # charcater.
     
     Args:
-    size (int): size of the square matrix
+        size (int): size of the square matrix
     Raises:
         TypeError: if size is not integer
         ValueError: if size is < 0
@@ -15,8 +15,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
     else:
         for i in range(size):
             for j in range(size):
