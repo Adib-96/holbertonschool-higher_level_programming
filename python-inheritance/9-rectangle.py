c3be7145f533df2b5e597de54e4d34e4f61e0a3c
adib <121905691+Adib-96@users.noreply.github.com>
2023-10-29 15:34:01 +0100

    Full rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,26 +2,26 @@
 """Defines a class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
-    def __init__(self,width,height):
-        """Initialize a Rectangle
-        
-        Args:
-            width (int): The width of the rectangle
-            height (int): The height of the rectangle
-        """
-        self.integer_validator("width",width)
-        self.integer_validator("height",height)
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
 
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Calculate the area of the rectangle"""
+        """Return the area of the rectangle."""
         return self.__width * self.__height
-    
+
     def __str__(self):
         """Return the print() and str() representation of a Rectangle."""
         string = "[" + str(self.__class__.__name__) + "] "
