33877084e1db71df89563960b79649af6114c590
Adib-96 <adibbh1996@gmail.com>
2023-11-11 15:26:04 +0100

    handle json format

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,11 +6,16 @@ Module with a Base class
 import json
 
 class Base:
-    """Base class"""
+    """
+    Base class
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new instance of the Base class"""
+        """
+        Initialize a new instance of the Base class
+        """
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
@@ -21,4 +26,5 @@ class Base:
     def to_json_string(list_dictionaries):
         if len(list_dictionaries) == 0 or list_dictionaries is None:
             return "[]"
+        
         return json.dumps(list_dictionaries)
