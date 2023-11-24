1151c44045f4a750cf9e17802776d63bf999cbea
Adib-96 <adibbh1996@gmail.com>
2023-11-24 21:14:36 +0100

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -31,16 +31,3 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return self.__size ** 2
-
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
