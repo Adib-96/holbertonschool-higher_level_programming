5bab80f18921ecb0ed9ce7cd7d01f94ade33ecb4
adib <121905691+Adib-96@users.noreply.github.com>
2023-11-28 19:40:23 +0100

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,44 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
 import sys
-
-
-def list_states(username, password, database):
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database)
-
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute the SQL query to retrieve states
-    query = "SELECT * FROM states ORDER BY states.id ASC"
-    cursor.execute(query)
-
-    # Fetch all the rows
-    states = cursor.fetchall()
-
-    # Display results
-    for state in states:
-        print(state)
-
-    # Close cursor and database connection
-    cursor.close()
-    db.close()
-
+import MySQLdb
 
 if __name__ == "__main__":
-    # Check if correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
-
-    # Get MySQL credentials from command line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Call the function to list states
-    list_states(username, password, database)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
+    
