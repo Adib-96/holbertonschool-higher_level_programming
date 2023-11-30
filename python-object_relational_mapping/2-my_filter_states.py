f80bfd066fafb5df1bdd431dbb3e48fb39a9a7df
Adib-96 <adibbh1996@gmail.com>
2023-11-30 14:15:50 +0100

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-"""
-a script that takes in an argument
-and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
-"""
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
 
-
-from MySQLdb import connect
-from sys import argv
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    """conncet to db and run the query then print the result"""
-    db = connect(
-        user=argv[1],
-        password=argv[2],
-        database=argv[3],
+    # Get MySQL credentials and state name from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Establish a connection to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name,
     )
     cursor = db.cursor()
-    cursor.execute(
-        "SELECT * FROM states  \
-            WHERE BINARY `name` = '{}'\
-                ".format(argv[4]))
-    [print(state) for state in cursor.fetchall()]
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
