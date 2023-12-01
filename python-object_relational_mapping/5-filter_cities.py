2f8890f015c4e5df8c2bc34a462a148ac2f9a0f8
Adib-96 <adibbh1996@gmail.com>
2023-12-01 08:50:24 +0100

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
-""" a script that takes in the name of a state
-as an argument and lists all cities of that state"""
-from MySQLdb import connect
+"""
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
+
+import MySQLdb
 from sys import argv
 
+if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
 
-if __name__ == "__main__":
-    """conncet to db and run the query then print the result"""
-    db = connect(
-        host="localhost",
-        user=argv[1],
-        password=argv[2],
-        database=argv[3],
-        port=3306
-    )
-    cur = db.cursor()
-    ph1 = "SELECT DISTINCT name FROM cities WHERE state_id IN"
-    ph2 = "(SELECT id FROM states WHERE name = %(statename)s)"
-    cur.execute(ph1 + " " + ph2, {'statename': argv[4]})
-    res = cur.fetchall()
-    ch = ""
-    if len(res) > 0:
-        for row in res:
-            ch += str(row[0]) + ", "
-        print(ch[:len(ch) - 2])
-    else:
-        print()
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        print(", ".join([row[1] for row in rows]))
