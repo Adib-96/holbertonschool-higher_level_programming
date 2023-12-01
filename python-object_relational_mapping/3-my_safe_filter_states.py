828718733a2cfa5b2c38bb4743ff83d17861b977
Adib-96 <adibbh1996@gmail.com>
2023-12-01 09:19:24 +0100

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,26 +14,23 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
 
-        # Create a cursor object to execute queries
-        cursor = db.cursor()
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
 
-        # Execute the SQL query with parameterized inputs to prevent SQL injection
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cursor.execute(query, (state_name,))
+    # Execute the SQL query with parameterized inputs to prevent SQL
+    # injection
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
 
-        # Fetch all the rows and display the results
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-
-    finally:
-        # Close the database connection
-        if db:
-            db.close()
+    # Fetch all the rows and display the results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
