642e1b20d7bafdd0b6b9c814f49308b157702ae0
Adib-96 <adibbh1996@gmail.com>
2024-02-01 14:37:07 +0100

    task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function () {
+  const updateHeader = document.querySelector('#update_header');
+  const header = document.querySelector('header');
+  updateHeader.addEventListener('click', function () {
+    header.textContent = 'new Header!!!';
+  });
+});
